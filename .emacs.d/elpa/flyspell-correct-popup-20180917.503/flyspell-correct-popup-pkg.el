1585bdab4802c0a2a6d6f06083e9bc7a37664552
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2018-09-25 17:21:17 -0400

    Added Emacs config

---
+++ .emacs.d/elpa/flyspell-correct-popup-20180917.503/flyspell-correct-popup-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flyspell-correct-popup" "20180917.503" "correcting words with flyspell via popup interface" '((flyspell-correct "0.4.0") (popup "0.5.3")) :commit "f67bcc639b43046c7aa5b7df1ef79fc3b5a887b4" :authors '(("Boris Buliga" . "boris@d12frosted.io")) :maintainer '("Boris Buliga" . "boris@d12frosted.io") :url "https://github.com/d12frosted/flyspell-correct")
---
+++ .emacs.d/elpa/simpleclip-20180811.1608/simpleclip-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "simpleclip" "20180811.1608" "Simplified access to the system clipboard" 'nil :commit "7fff9a1e574466878b5f91e9b56b16e490045aaa" :keywords '("convenience") :authors '(("Roland Walker" . "walker@pobox.com")) :maintainer '("Roland Walker" . "walker@pobox.com") :url "http://github.com/rolandwalker/simpleclip")
